db19872e3aaf334d394114c32353e0560c144f56
Uruskhan Bokov <bokovuruskhan@gmail.com>
2023-01-06 04:07:13 +0300

    update services (SprintService, TaskService) add methods (get active sprint, get active sprint completed/opened tasks, get active sprint high/low priority completed/opened tasks)

--- src/main/java/root/timemanagementapp/service/SprintService.java
+++ src/main/java/root/timemanagementapp/service/SprintService.java
@@ -6,14 +6,24 @@ import root.timemanagementapp.database.model.Sprint;
 import root.timemanagementapp.database.repo.SprintRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SprintService {
     @Autowired
     private SprintRepository sprintRepository;
 
-    public List<Sprint> getAllSprints(){
+    public List<Sprint> getAllSprints() {
         return sprintRepository.findAll();
+    }
+
+    public Sprint getActiveSprint() throws Exception {
+        Optional<Sprint> optionalSprint = getAllSprints().stream().filter(Sprint::isActive).findFirst();
+        if (optionalSprint.isPresent()) {
+            return optionalSprint.get();
+        } else {
+            throw new Exception("No active sprint");
+        }
     }
 
 }
--- src/main/java/root/timemanagementapp/service/TaskService.java
+++ src/main/java/root/timemanagementapp/service/TaskService.java
@@ -6,14 +6,42 @@ import root.timemanagementapp.database.model.Task;
 import root.timemanagementapp.database.repo.TaskRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
     @Autowired
     private TaskRepository taskRepository;
 
-    public List<Task> getAllTasks(){
+    @Autowired
+    private SprintService sprintService;
+
+    public List<Task> getAllTasks() {
         return taskRepository.findAll();
+    }
+
+    public List<Task> getActiveSprintCompletedTasks() throws Exception {
+        return sprintService.getActiveSprint().getTasks().stream().filter(Task::isCompleted).collect(Collectors.toList());
+    }
+
+    public List<Task> getActiveSprintOpenedTasks() throws Exception {
+        return sprintService.getActiveSprint().getTasks().stream().filter(Task-> !Task.isCompleted()).collect(Collectors.toList());
+    }
+
+    public List<Task> getCurrentSprintHighPriorityCompletedTasks() throws Exception {
+        return getActiveSprintCompletedTasks().stream().filter(Task::isHighPriority).collect(Collectors.toList());
+    }
+
+    public List<Task> getCurrentSprintHighPriorityOpenedTasks() throws Exception {
+        return getActiveSprintOpenedTasks().stream().filter(Task::isHighPriority).collect(Collectors.toList());
+    }
+
+    public List<Task> getCurrentSprintLowPriorityCompletedTasks() throws Exception {
+        return getActiveSprintCompletedTasks().stream().filter(Task->!Task.isHighPriority()).collect(Collectors.toList());
+    }
+
+    public List<Task> getCurrentSprintLowPriorityOpenedTasks() throws Exception {
+        return getActiveSprintOpenedTasks().stream().filter(Task->!Task.isHighPriority()).collect(Collectors.toList());
     }
 
 }
