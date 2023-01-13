33042e9f6a465ae7abd67ffea897ef1c325eb71b
Uruskhan Bokov <bokovuruskhan@gmail.com>
2023-01-13 13:57:04 +0300

    add live clock

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -35,3 +35,20 @@ function addTask(){
     const jsonData = Object.fromEntries(data.entries());
     sendRequest("/task","POST",jsonData,true);
 };
+function clockTimer()
+{
+  var date = new Date();
+
+  var time = [date.getHours(),date.getMinutes(),date.getSeconds()];
+  var days = date.getDay();
+
+  if(time[0] < 10){time[0] = "0"+ time[0];}
+  if(time[1] < 10){time[1] = "0"+ time[1];}
+  if(time[2] < 10){time[2] = "0"+ time[2];}
+
+  var currentTime = date.getDate() + "." + date.getMonth() + 1+ "."+ date.getFullYear() +" "+ [time[0],time[1],time[2]].join(':');
+  var clock = document.getElementById("clock");
+  console.log(currentTime);
+  clock.innerHTML = currentTime;
+  setTimeout("clockTimer()", 1000);
+};
