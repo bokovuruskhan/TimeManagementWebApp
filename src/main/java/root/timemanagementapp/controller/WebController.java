fee3f2aa490d5c25a597554f9cc331932e57346e
Uruskhan Bokov <bokovuruskhan@gmail.com>
2023-01-16 05:13:45 +0300

    add statistics

--- src/main/java/root/timemanagementapp/controller/WebController.java
+++ src/main/java/root/timemanagementapp/controller/WebController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import root.timemanagementapp.database.model.User;
 import root.timemanagementapp.dto.UserDto;
 import root.timemanagementapp.service.SprintService;
+import root.timemanagementapp.service.StatisticsService;
 import root.timemanagementapp.service.TaskService;
 import root.timemanagementapp.service.UserService;
 
@@ -22,13 +23,16 @@ public class WebController {
     @Autowired
     private TaskService taskService;
 
+    @Autowired
+    private StatisticsService statisticsService;
+
     @GetMapping("/login")
     public String showLoginView(Model model) {
         return "login";
     }
 
     @RequestMapping("/dev")
-    public String showDevView(@RequestParam("devId") Long devId,Model model) throws Exception {
+    public String showDevView(@RequestParam("devId") Long devId,Model model) {
         User developer = userService.findUserById(devId);
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("lowPriorityCompletedTasks", taskService.getActiveSprintLowPriorityCompletedTasksByUserId(developer.getId()));
@@ -37,6 +41,7 @@ public class WebController {
         model.addAttribute("highPriorityOpenedTasks", taskService.getActiveSprintHighPriorityOpenedTasksByUserId(developer.getId()));
         model.addAttribute("activeSprint", sprintService.getActiveSprint());
         model.addAttribute("developers", userService.findAllDevelopers());
+        model.addAttribute("statistics", statisticsService.getActiveSprintStatistics());
         UserDto currentUserDto = new UserDto();
         currentUserDto.setId(currentUser.getId());
         currentUserDto.setUsername(currentUser.getUsername());
@@ -47,7 +52,7 @@ public class WebController {
     }
 
     @RequestMapping("/")
-    public String index(Model model) throws Exception {
+    public String index(Model model) {
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         model.addAttribute("lowPriorityCompletedTasks", taskService.getActiveSprintLowPriorityCompletedTasksByUserId(currentUser.getId()));
         model.addAttribute("highPriorityCompletedTasks", taskService.getActiveSprintHighPriorityCompletedTasksByUserId(currentUser.getId()));
@@ -55,6 +60,7 @@ public class WebController {
         model.addAttribute("highPriorityOpenedTasks", taskService.getActiveSprintHighPriorityOpenedTasksByUserId(currentUser.getId()));
         model.addAttribute("activeSprint", sprintService.getActiveSprint());
         model.addAttribute("developers", userService.findAllDevelopers());
+        model.addAttribute("statistics", statisticsService.getActiveSprintStatistics());
         UserDto currentUserDto = new UserDto();
         currentUserDto.setId(currentUser.getId());
         currentUserDto.setUsername(currentUser.getUsername());
--- src/main/java/root/timemanagementapp/dto/StatisticsDto.java
+++ src/main/java/root/timemanagementapp/dto/StatisticsDto.java
@@ -1,7 +1,44 @@
 package root.timemanagementapp.dto;
 
+import root.timemanagementapp.database.model.Sprint;
+import root.timemanagementapp.database.model.Task;
+
 public class StatisticsDto {
 
-    private Double x;
+    private final long x;
 
+    private final long y;
+
+    private final long c1;
+    private final long c2;
+
+    private final long e;
+
+    public StatisticsDto(Sprint sprint) {
+        x = sprint.getTasks().stream().filter(Task -> !Task.getCompleted()).map(TaskDto::new).mapToLong(TaskDto::getEstimatedHours).sum();
+        y = sprint.getTasks().stream().filter(Task -> !Task.getCompleted()).map(TaskDto::new).mapToLong(TaskDto::getElapsedHours).sum();
+        c1 = sprint.getTasks().stream().map(Task::getUser).count();
+        c2 = sprint.getTasks().size();
+        e = sprint.getTasks().stream().map(TaskDto::new).mapToLong(TaskDto::getEstimatedHours).sum();
+    }
+
+    public long getX() {
+        return x;
+    }
+
+    public long getY() {
+        return y;
+    }
+
+    public long getC1() {
+        return c1;
+    }
+
+    public long getC2() {
+        return c2;
+    }
+
+    public long getE() {
+        return e;
+    }
 }
